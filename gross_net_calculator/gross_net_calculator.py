7ca95f3f45af1c5ba6f34c1f06947410c6f90f08
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-03-14 12:56:47 -0400

    fixed gnc's output, and the output format test works.

--- gross_net_calculator/gross_net_calculator.py
+++ gross_net_calculator/gross_net_calculator.py
@@ -40,7 +40,9 @@ def gross_net_calculator(dollars, fed=0, state=0, penalty=0):
     penalty = Decimal(penalty)
 
     #GrossNet = namedtuple('GrossNet', 'gross net fed_dollars state_dollars penalty_dollars')
-    GrossNet = namedtuple('GrossNet', 'gross net')
+    #GrossNet = namedtuple('GrossNet', 'gross net')
+    Gn_shell_type = namedtuple('Gross_Net_Shell', 'input_was_gross input_was_net')
+    Gn_output_type = namedtuple('Gross_Net_Output', 'output_dollars federal state fed_penalty summary')
 
     # convert to decimal represenation of the percentage
     fed = fed / 100
@@ -54,15 +56,60 @@ def gross_net_calculator(dollars, fed=0, state=0, penalty=0):
     # TODO: label the tax dollar amounts better,
     #       because it will depend on if the input was gross or net
     #       so I'll have two sets of dollar amounts in the output.
+
+    # create a named tuple in the custom output format that will show all related info
+    # when assuming the input dollars is the gross amount
+    input_was_gross = Gn_output_type(Decimal(net).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                     Decimal(dollars * fed).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                     Decimal(dollars * state).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                     Decimal(dollars * penalty).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                     '''Assuming the input dollar value (${dollars:.2f}) is the Gross amount,
+the Net amount is ${net:.2f},
+{fed_pct:.2f}% withheld for Federal taxes is ${fed_dollars:.2f},
+{state_pct:.2f}% withheld for State taxes is ${state_dollars:.2f},
+and a {penalty_pct:.2f}% Federal Penalty tax is ${penalty_dollars:.2f}.'''.format(dollars=dollars,
+                                                                                  net=net,
+                                                                                  fed_pct=fed * 100,
+                                                                                  fed_dollars=dollars * fed,
+                                                                                  state_pct=state * 100,
+                                                                                  state_dollars=dollars * state,
+                                                                                  penalty_pct=penalty * 100,
+                                                                                  penalty_dollars=dollars * penalty,
+                                                                                 ),
+                                    )
+    # create a named tuple in the custom output format that will show all related info
+    # when assuming the input dollars is the net amount
+    input_was_net = Gn_output_type(Decimal(gross).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                   Decimal(gross * fed).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                   Decimal(gross * state).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                   Decimal(gross * penalty).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                   '''Assuming the input dollar value (${dollars:.2f}) is the Net amount,
+the Gross amount is ${gross:.2f},
+{fed_pct:.2f}% withheld for Federal taxes is ${fed_dollars:.2f},
+{state_pct:.2f}% withheld for State taxes is ${state_dollars:.2f},
+and a {penalty_pct:.2f}% Federal Penalty tax is ${penalty_dollars:.2f}.'''.format(dollars=dollars,
+                                                                                  gross=gross,
+                                                                                  fed_pct=fed * 100,
+                                                                                  fed_dollars=gross * fed,
+                                                                                  state_pct=state * 100,
+                                                                                  state_dollars=gross * state,
+                                                                                  penalty_pct=penalty * 100,
+                                                                                  penalty_dollars=gross * penalty,
+                                                                                 ),
+                                  )
+    shell = Gn_shell_type(input_was_gross, input_was_net)
+
+    return shell
+
 ##    return GrossNet(gross.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
 ##                    net.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
 ##                    Decimal(gross * fed).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
 ##                    Decimal(gross * state).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
 ##                    Decimal(gross * penalty).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
 ##                    )
-    return GrossNet(gross.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                    net.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                    )
+##    return GrossNet(gross.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+##                    net.quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+##                    )
 
 
 
@@ -74,9 +121,12 @@ if __name__ == '__main__':
 
     ans = gross_net_calculator(dollars, fed, state, penalty)
 
-    print('\nTax rates used: Fed={}%, State={}%, Penalty={}%'.format(fed, state, penalty))
-    print('\nnet({}) -> gross({})\n\nIf the desired net amount is {} dollars, then gross amount needed is {}'.format(dollars, ans.gross, dollars, ans.gross))
-    print('\ngross({}) -> net({})\n\nIf a gross amount of {} dollars is requested, the net amount will be {}'.format(dollars, ans.net, dollars, ans.net))
-    print('ans={}'.format(ans))
+    print(ans.input_was_gross.summary)
+
+##
+##    print('\nTax rates used: Fed={}%, State={}%, Penalty={}%'.format(fed, state, penalty))
+##    print('\nnet({}) -> gross({})\n\nIf the desired net amount is {} dollars, then gross amount needed is {}'.format(dollars, ans.gross, dollars, ans.gross))
+##    print('\ngross({}) -> net({})\n\nIf a gross amount of {} dollars is requested, the net amount will be {}'.format(dollars, ans.net, dollars, ans.net))
+##    print('ans={}'.format(ans))
     input('Press <ENTER> to quit.')
 
--- gross_net_calculator/gross_net_calculator_test1.py
+++ gross_net_calculator/gross_net_calculator_test1.py
@@ -23,13 +23,37 @@ class Test_gross_net_calculator_test1(unittest.TestCase):
                                          Decimal(1333.33).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
                                          Decimal(222.22).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
                                          Decimal(0).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                                         'Assuming the input dollar value ($5555.56) is the Gross amount,\nthe Net amount is $4000.00,\nthe 24% Federal tax will be $1333.33,\nthe 4% State tax will be $222.22,\nand the 0% Federal Penalty will be $0.',
+                                         '''Assuming the input dollar value (${dollars:.2f}) is the Gross amount,
+the Net amount is ${net:.2f},
+{fed_pct:.2f}% withheld for Federal taxes is ${fed_dollars:.2f},
+{state_pct:.2f}% withheld for State taxes is ${state_dollars:.2f},
+and a {penalty_pct:.2f}% Federal Penalty tax is ${penalty_dollars:.2f}.'''.format(dollars=5555.56,
+                                                                                  net=4000,
+                                                                                  fed_pct=24.00,
+                                                                                  fed_dollars=1333.33,
+                                                                                  state_pct=4.00,
+                                                                                  state_dollars=222.22,
+                                                                                  penalty_pct=0.00,
+                                                                                  penalty_dollars=0.00,
+                                                                                 ),
                                         )
         input_was_net = Gn_output_type(Decimal(7716.06).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                                       Decimal().quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                                       Decimal().quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                       Decimal(1851.8544).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
+                                       Decimal(308.64).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
                                        Decimal(0).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN),
-                                       'Assuming the input dollar value ($5555.56) is the Net amount,\nthe Gross amount is $7716.06,\nthe 24% Federal tax will be $1851.85,\nthe 4% State tax will be $308.64,\nand the 0% Federal Penalty will be $0.',
+                                       '''Assuming the input dollar value (${dollars:.2f}) is the Net amount,
+the Gross amount is ${gross:.2f},
+{fed_pct:.2f}% withheld for Federal taxes is ${fed_dollars:.2f},
+{state_pct:.2f}% withheld for State taxes is ${state_dollars:.2f},
+and a {penalty_pct:.2f}% Federal Penalty tax is ${penalty_dollars:.2f}.'''.format(dollars=5555.56,
+                                                                                  gross=7716.06,
+                                                                                  fed_pct=24.00,
+                                                                                  fed_dollars=1851.85,
+                                                                                  state_pct=4.00,
+                                                                                  state_dollars=308.64,
+                                                                                  penalty_pct=0.00,
+                                                                                  penalty_dollars=0.00,
+                                                                                 ),
                                       )
         shell = Gn_shell_type(input_was_gross, input_was_net)
 
@@ -37,17 +61,17 @@ class Test_gross_net_calculator_test1(unittest.TestCase):
         
          
     
-    def test_gross_net_calculator_known_answers(self):
-        known_answers_gross_net_calculator = [
-                ((5555.56, 24, 4, 0),
-                 ((Decimal(7716.06).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN), )),
-                  (Decimal(4000).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN), )
-                 )
-                ]
-        for case, ans in known_answers_gross_net_calculator:
-            test_ans = gross_net_calculator(dollars=case[0], fed=case[1], state=case[2], penalty=case[3])
-            print('test_ans={}\nans={}'.format(test_ans, ans))
-            self.assertEqual(test_ans, ans)
+##    def test_gross_net_calculator_known_answers(self):
+##        known_answers_gross_net_calculator = [
+##                ((5555.56, 24, 4, 0),
+##                 ((Decimal(7716.06).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN), )),
+##                  (Decimal(4000).quantize(Decimal('.01'), rounding=ROUND_HALF_DOWN), )
+##                 )
+##                ]
+##        for case, ans in known_answers_gross_net_calculator:
+##            test_ans = gross_net_calculator(dollars=case[0], fed=case[1], state=case[2], penalty=case[3])
+##            print('test_ans={}\nans={}'.format(test_ans, ans))
+##            self.assertEqual(test_ans, ans)
 
 if __name__ == '__main__':
     unittest.main()
